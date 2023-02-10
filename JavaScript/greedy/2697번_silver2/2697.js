6df84e466862c5f67d8f0f66818121287bdc00af
YoungSeungJang <1345620@naver.com>
2023-02-11 02:16:38 +0900

    FIX:2697_silver2

--- JavaScript/greedy/2697번_silver2/2697.js
+++ JavaScript/greedy/2697번_silver2/2697.js
@@ -26,13 +26,57 @@ A와 B의 구성이 같다는 말은 A를 이루고 있는 각 자리수의 등�
 BIGGEST
 */
 
-
 // 생각접근
 // 13542 라는 숫자가 있다고 가정해보자
 // 14235 가 13542 보다 크면서 제일 작은 수이다.
 
 // 내림차순이 시작되는 지점을 먼저찾는다. 위를예시로 13 / 542 이다
-// 
+// 시작되는 지점전(3) 과 후(542) 에서 (3)보다 큰수중에 제일 작은 수를 (542)에서 찾은뒤 바꿔준다
+// 14 / 532 에서 532 는 오름차순으로 정렬해준다
+// 14 / 235 둘을 합치면된다.
 
-let input = 2791343997423;
+let input = '279134399742';
 
+let index = 0;
+input = input.split('');
+
+
+for (let i = input.length - 1; i >= 1; i--) {
+  if (input[i - 1] < input[i]) {
+    index = i - 1;
+    break;
+  }
+}
+
+if(index == 0){
+    console.log('BIGGEST')
+    return;
+}
+let newArr = input.splice(index).map((item) => Number(item));
+
+
+let temp = newArr[0];
+let count = newArr[0];
+let pass = true;
+let tempIdx = 0;
+while (pass) {
+  count++;
+  countIdx = newArr.findIndex(item=>item==count);
+  if(countIdx>0){
+    newArr[0] = newArr[countIdx];
+    newArr[countIdx] = temp;
+    pass = false;
+  }
+}
+
+let sortArr = newArr.splice(1).sort((a,b)=>a-b);
+let newSortArr = newArr.concat(sortArr).map(item=>String(item));
+
+let result = input.concat(newSortArr);
+
+input = result.join('');
+console.log(input);
+
+
+// 뭔가 식이 조금 조잡하지만 생각접근대로 나름 진행하여 풀이하였다.
+// 더 간결하게 풀 수 있을거같지만 일단 내방식대로 풀이했다는게 굉장히 잘해냈다고 생각한다
